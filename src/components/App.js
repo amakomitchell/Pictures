ce95bc55b5a7a82349c624b37d78be857b2d592e
Ugochi <amakomitchell5@gmail.com>
2020-01-09 15:57:03 +0100

    Handle get request with Async Await

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 
+                    'Client-ID e1cad4f3aafc5029cb66b1835fec6702568edd5380b294687b8f80347a3ba14d'
+            }
+        });
+        console.log(response.data.results);
     }
     render() {
         return (
